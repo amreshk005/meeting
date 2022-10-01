74a437c5b8e90fc702a1dd9ab415a608927f1e8d
ABHISHEK KUMAR <amreshk005@gmail.com>
2022-10-01 18:44:44 +0530

    made responsive

--- src/components/Meeting.jsx
+++ src/components/Meeting.jsx
@@ -9,11 +9,7 @@ import Divider from "@mui/material/Divider";
 
 function Meeting() {
   return (
-    <Box
-      sx={{
-        padding: 10,
-      }}
-    >
+    <Box className={style.container}>
       <Paper>
         <Grid className={style.meetingContainer} container spacing={2}>
           <Grid className={style.leftContainer} xs={12} md={5} lg={5}>
@@ -40,14 +36,16 @@ function Meeting() {
           </Grid>
           <Divider classname={style.h100} orientation="vertical" flexItem />
 
-          <Grid className={style.rightContainer} item xs={12} md={6} lg={6}>
+          <Grid className={style.rightContainer} xs={12} md={6} lg={6}>
             <h4 className={style.formHeading}>Enter Details</h4>
             <Box>
               <Box sx={{ marginBottom: 2.5 }}>
                 <Box sx={{ marginBottom: 1 }}>
                   <label>Name *</label>
                 </Box>
-                <input type="text" className={style.input} />
+                <Box sx={{ marginBottom: 1 }}>
+                  <input type="text" className={style.input} />
+                </Box>
               </Box>
               <Box sx={{ marginBottom: 2.5 }}>
                 <Box sx={{ marginBottom: 1 }}>
@@ -57,7 +55,7 @@ function Meeting() {
               </Box>
               <Box sx={{ marginBottom: 2.5 }}>
                 <Button className={style.button} variant="outlined">
-                  Outlined
+                  Add Guests
                 </Button>
               </Box>
               <Box sx={{ marginBottom: 2.5 }}>
